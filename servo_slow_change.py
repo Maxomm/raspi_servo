3a94897cd7fe2a0914d2dc3fe959de8b05bbff47
Maxomm <max.warsinke@googlemail.com>
2022-10-12 16:54:58 +0200

    test

--- servo_slow_change.py
+++ servo_slow_change.py
@@ -1,30 +1,30 @@
 import socket
 import time
-
-# from adafruit_servokit import ServoKit
+from adafruit_servokit import ServoKit
 
 PORT = 12345
-HOST = "127.0.0.1"
-STEPS = 10
-# kit = ServoKit(channels=16)
+# LOCAL
+HOST = "192.168.1.1"
+# HOST = "127.0.0.1"
+STEPS = 5
+kit = ServoKit(channels=16)
 
-"""
+
 s_list = [
     [kit.servo[10], kit.servo[11]],
-    [kit.servo[12], kit.servo[13]],
+    [kit.servo[13], kit.servo[12]],
     [kit.servo[15], kit.servo[14]],
 ]
-"""
-s_list = [[0, 0], [0, 0], [0, 0]]
+
 
 states = {
-    "Neutral": [60, 60, 60],
-    "Angry": [0, 60, 110],
+    "Neutral": [20, 20, 20],
+    "Angry": [110, 60, 0],
     "Sad": [110, 110, 110],
     "Surprised": [0, 0, 0],
-    "Disgusted": [110, 60, 0],
-    "Happy": [10, 50, 100],
-    "Fearful": [0, 30, 60],
+    "Disgusted": [0, 60, 110],
+    "Happy": [60, 60, 60],
+    "Fearful": [60, 30, 60],
 }
 
 
@@ -45,11 +45,20 @@ def move_servo(pos):
         s_pair[1] = reverse(pos[index])
 
 
+def correct_value(angle):
+    if angle < 0:
+        return 0
+    elif angle > 120:
+        return 120
+    else:
+        return angle
+
+
 def move_servos_slow(goal):
     range_list = [0, 0, 0]
     factor_list = [0, 0, 0]
     for i in range(3):
-        range_list[i] = goal[i] - s_list[i][0]
+        range_list[i] = goal[i] - s_list[i][0].angle
         if range_list[i] != 0:
             factor_list[i] = STEPS / abs(range_list[i])
         else:
@@ -58,10 +67,13 @@ def move_servos_slow(goal):
     for _ in range(STEPS):
         for i in range(3):
             if factor_list[i] != 0:
-                s_list[i][0] += mysign(range_list[i]) / factor_list[i]
-                s_list[i][1] = reverse(s_list[i][0])
-        print(s_list)
-        time.sleep(0.1)
+                current_angle = int(s_list[i][0].angle)
+                current_angle += mysign(range_list[i]) / factor_list[i]
+                current_angle = correct_value(current_angle)
+                s_list[i][0].angle = current_angle
+                s_list[i][1].angle = reverse(current_angle)
+        # print(s_list)
+        time.sleep(0.05)
 
 
 def start():
@@ -85,3 +97,4 @@ def start():
 
 if __name__ == "__main__":
     start()
+    
